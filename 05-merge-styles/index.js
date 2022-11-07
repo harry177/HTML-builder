5137a53d4d70ae79d3bf8e3cdbbcd2fd3a107ff5
Harry177 <harry177@rambler.ru>
2022-11-07 21:18:00 +0100

    feat: complete 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+// Defining pathes
+
+const sourceStylePath = path.join(__dirname, 'styles');
+const bundleStylePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+// Creating WriteStream
+
+const writeStream = fs.createWriteStream(bundleStylePath);
+
+// Reading source css-files
+
+fs.readdir(sourceStylePath, (error, files) => {
+    if (error) throw error;
+
+    files.forEach(data => {
+        if(path.extname(data) === '.css') {
+
+            const streamPath = path.join(__dirname, 'styles', data);
+
+            // Creating ReadStream
+
+            const readStream = fs.createReadStream(streamPath);
+
+            // Piping data from source to dist
+            
+            readStream.pipe(writeStream);
+        }
+    })
+
+    
+})
